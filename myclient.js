afe62faa6728c17b3f45f0175871ef6ea31adbc2
Esteban Herrera <esteban.herrera@carpoolagency.com>
2016-02-01 09:49:22 -0800

    added control buttons

--- myclient.js
+++ myclient.js
@@ -1,40 +1,45 @@
-window.onload = function () {
+window.onload = function() {
   var url,
       jqxhr;
 
-    url1 = document.URL + 'players/0';
-    url2 = document.URL + 'players/1';
-    jqxhr = $.getJSON(url1, function(data) {
+  url1 = document.URL + 'players/0';
+  url2 = document.URL + 'players/1';
+  jqxhr = $.getJSON(url1, function(data) {
       console.log('API for P1 response received');
 
-      $('#player1Name').html('Player ' + data['name']);
-      $('#player1Score').html(data['score'] + ' points');
-    });
+      $('#player1Name').html(data['name']);
+      $('#player1Score').html(data['score']);
+  });
 
-    jqxhr = $.getJSON(url2, function(data) {
+  jqxhr = $.getJSON(url2, function(data) {
       console.log('API for P2 response received');
-      $('#player2Name').html('Player ' + data['name']);
-      $('#player2Score').html(data['score'] + ' points');
-    });
+      $('#player2Name').html(data['name']);
+      $('#player2Score').html(data['score']);
+  });
 };
-
 
 function increaseScore(id) {
   var url = document.URL + 'players/' + id + '/increaseScore';
   $.ajax({
-    url: url,
-    type: 'PUT',
-    success: function(result) {
-      console.log('Increased the score!');
-      updateScore(id);
-    }
+      url: url,
+      type: 'PUT',
+      success: function(result) {
+          console.log('Increased the score!');
+          // Update the score in HTML
+          $('#player' + (id + 1) + 'Score').hide().html(result).fadeIn('fast');
+      }
   });
 }
 
-function updateScore(id) {
-  var url = document.URL + 'players/' + id;
-  $.getJSON(url, function(data) {
-    $('#player' + id + 'Score').hide().html(data['score']).fadeIn('fast');
+function decreaseScore(id) {
+  var url = document.URL + 'players/' + id + '/decreaseScore';
+  $.ajax({
+      url: url,
+      type: 'PUT',
+      success: function(result) {
+          console.log('Increased the score!');
+          // Update the score in HTML
+          $('#player' + (id + 1) + 'Score').hide().html(result).fadeIn('fast');
+      }
   });
-} 
-
+}
--- scoreboardServer.js
+++ scoreboardServer.js
@@ -1,48 +1,61 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-var players = [{ name: 'Esteban', score: 0 },
-              { name: 'Other Player', score: 0 }];
+var players = [{
+    name: 'Player 1',
+    score: 0
+}, {
+    name: 'Player 2',
+    score: 0
+}];
 
-app.use(express['static'](__dirname ));
+app.use(express['static'](__dirname));
 
 // Express route for incoming requests for a customer name
 app.get('/players/:id', function(request, response) {
-  response.status(200).send(players[request.params.id]);
+    response.status(200).send(players[request.params.id]);
 });
 
 app.put('/players/:id/increaseScore', function(request, response) {
-  console.log("request to increase score of player with id", request.params.id);
-  if(request.params.id != 1 && request.params.id  != 0) {
-    // 406 status is 'not acceptable' https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
-    console.log("There's no player with this id");
-    response.status(406).send("There is no player with id: " + request.params.id);
-  } else {
-    console.log("Attempting to increase score");
-    players[request.params.id].score += 1;
-    response.status(200).send();  
-  }
+    console.log("request to increase score of player with id", request.params.id);
+    if (request.params.id != 1 && request.params.id != 0) {
+        // 406 status is 'not acceptable' https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+        console.log("There's no player with this id");
+        response.status(406).send("There is no player with id: " + request.params.id);
+    } else {
+        console.log("Attempting to increase score");
+        players[request.params.id].score += 1;
+        response.status(200).send(players[request.params.id].score.toString());
+    }
 });
 
 app.put('/players/:id/decreaseScore', function(request, response) {
-  console.log("going to decrease score of player ", players[request.params.id]);
-  response.status(200).send();  
+    console.log("request to decrease score of player with id", request.params.id);  
+    if (request.params.id != 1 && request.params.id != 0) {
+        // 406 status is 'not acceptable' https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+        console.log("There's no player with this id");
+        response.status(406).send("There is no player with id: " + request.params.id);
+    } else {
+        console.log("going to decrease score of player ", players[request.params.id]);
+        players[request.params.id].score -= 1;
+        response.status(200).send(players[request.params.id].score.toString());
+    }
 });
 
 //TODO: implement reset function
 
 // Express route for any other unrecognized incoming requests
 app.get('*', function(req, res) {
-  res.status(404).send('Unrecognized API call');
+    res.status(404).send('Unrecognized API call');
 });
 
 // Express route to handle errors
 app.use(function(err, req, res, next) {
-  if (req.xhr) {
-    res.status(500).send('Oops, something went wrong!');
-  } else {
-    next(err);
-  }
+    if (req.xhr) {
+        res.status(500).send('Oops, something went wrong!');
+    } else {
+        next(err);
+    }
 });
 
 app.listen(3000);
